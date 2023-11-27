eef8c49a47aa502b1871f314c8b97efb8f8f6d5a
fiZZowsky <piotrekr53@interia.pl>
2023-11-27 16:23:07 +0100

    fixed Play Again button and exec files

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -9,4 +9,9 @@ public class StartMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void ResetTheGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }
