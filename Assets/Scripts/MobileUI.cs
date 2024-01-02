4f022f36dc5c9b4ae094a7b5a97fefb8419f5158
fiZZowsky <piotrekr53@interia.pl>
2024-01-02 17:29:09 +0100

    Fixed mobile ui hidding on windows

--- Assets/Scripts/MobileUI.cs
+++ Assets/Scripts/MobileUI.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MobileUI : MonoBehaviour
 {
     [SerializeField] Joystick joystick;
+    [SerializeField] Button jumpButton;
 
     void Start()
     {
         if (Application.isMobilePlatform)
         {
             joystick.gameObject.SetActive(true);
+            jumpButton.gameObject.SetActive(true);
         }
         else
         {
             joystick.gameObject.SetActive(false);
+            jumpButton.gameObject.SetActive(false);
         }
     }
 }
